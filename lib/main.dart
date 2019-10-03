9265f59486f44a19f4387f3d8a5ea6a7695b716c
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-03 16:37:53 -0300

    Trabalhando com TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blueAccent
       ),
-      home: Scaffold(
+      home: HomePage()
+    );
+  }
+  
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text("Olá Flutter"),
         ),
         body: Container(
           color: Colors.white54 ,
+          child: Center(  
+            child: Text(
+              "Olá mundo !!!!!",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.lineThrough,
+                decorationColor: Colors.red,
+                decorationStyle: TextDecorationStyle.double
+
+              )),
+            
+            ),
         ),
         drawer: Container(
           color: Colors.red,
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (){},
-          child: Icon(Icons.perm_phone_msg),
+          child: Icon(Icons.signal_cellular_connected_no_internet_4_bar),
           ),
-      )
-    );
+      );
   }
 }
